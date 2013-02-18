91a5894cc44bf177417f2f2f0dcfef5876ce01c3
Simeon Franklin <simeonf@gmail.com>
2013-02-17 16:06:46 -0800

    Fabfile works

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,52 @@
-from fabric.api import env, run, task
+from fabric.api import env, run, sudo, task, cd, hide
 
 env.user = "ec2-user"
-env.key_filename = ["./templatekey.pem"]
+env.key_filename = ["~/.ssh/templatekey.pem"]
 
-# server setup,
-# easy_install virtualenv and activate ENV
-# sudo yum install -y mysql python-mysql python-imaging
-# pip install flask, etc
-# configure apache, install db
+@task
+def yum():
+    with hide('output'):
+        sudo("yum install -y mysql mysql-server")
+        sudo("yum install -y MySQL-python python-imaging")
+        sudo("yum install -y httpd mod_wsgi")
+        sudo("yum install -y git")
+    sudo("/etc/init.d/mysqld start")
+    sudo("/etc/init.d/httpd start")
 
+@task
+def clear_app_dir():
+    sudo('rm -rf /var/www/instaclone')
+
+@task
+def populate_db():
+    with cd('/var/www/instaclone/instaclone'):
+        run('/var/www/instaclone/ENV/bin/python populate_db.py ./sample-images/')
+    
+@task
+def setup_app():
+    sudo('mkdir /var/www/instaclone')
+    sudo('chown ec2-user:ec2-user /var/www/instaclone')
+    sudo('easy_install virtualenv')
+    with cd('/var/www/instaclone'):
+        run('virtualenv --system-site-packages ENV')
+        with hide('output'):
+            run('ENV/bin/pip install flask flask-mysql')
+            run('git clone git://github.com/simeonf/instaclone.git')
+        #run('mkdir instaclone/static/uploads')
+        run('ln -s /var/www/instaclone/instaclone ENV/lib/python2.6/site-packages/')
+        # setup apache
+        response = sudo('ls /etc/httpd/conf/httpd.conf.old', quiet=True)
+        if response.return_code > 0: # ls failed
+            sudo("cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.old")
+        sudo("cp /etc/httpd/conf/httpd.conf.old httpd.conf")
+        sudo("cat instaclone/instaclone.conf >> httpd.conf")
+        sudo("cp httpd.conf /etc/httpd/conf/httpd.conf")
+        sudo("/etc/init.d/httpd restart")
+        # setup mysql
+        run("mysql -u root < instaclone/schema.mysql")
+
+@task
+def setup():
+    yum()
+    setup_app()
+    populate_db()
